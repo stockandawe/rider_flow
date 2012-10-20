90e32bd585ae9dd0eeba38d80c34856525b96df7
Rutul Dave <rutul98@yahoo.com>
2012-10-20 00:13:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiderFlow::Application.config.secret_token = '47993617032510ff0112165e823222af384e036645126a6f89208e699177382739af9dd31829e00723ccc8e5a8d577f8ff3e0fc4c0c70ad7e9f35c716805ff85'
