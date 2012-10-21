24ffda06be681df131d01c96729cca2ba85ecda6
Rutul Dave <rutul98@yahoo.com>
2012-10-20 21:12:14 -0700

    Update the bus coordinate from the live feed

--- app/controllers/cron_controller.rb
+++ app/controllers/cron_controller.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+
 class CronController < ApplicationController
   def riders
     Stop.all.each do |s|
@@ -10,5 +13,19 @@ class CronController < ApplicationController
       new_riders = -5 + rand(11)
       b.update_attribute(:riders, b.riders + (new_riders < 0 ? 0 : new_riders))
     end
+    
+    # update the bus coordinates from the feed
+    route_tag = "2"
+    url_for_buses_route_tag = "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni&r="+route_tag
+    data = Nokogiri::XML(open(url_for_buses_route_tag))
+    root = data.root
+    buses = root.xpath('//vehicle')
+    
+    buses.each do |bus|
+      bus_id = bus.attr('id')
+      b = Bus.find_by_bus_id(bus_id)
+      b.update_attribute(:lat, bus.attr('lat'))
+      b.update_attribute(:long, bus.attr('lon')) 
+    end    
   end
 end
